4c0b50290c354a0858a64d427a1435c64af0e799
Alex Nedelkovski <ace_nedelkovski@hotmail.com>
2019-11-19 14:05:26 -0600

    used Material to add navigation

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -6,14 +6,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
-  
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
